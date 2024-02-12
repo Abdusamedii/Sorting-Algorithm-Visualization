b8280da1e2db49be5184a0c30433233e0c716b58
Abdusamed Beqiri <beqiriabdusamed@gmail.com>
2024-02-12 16:56:55 +0100

    css issues and sound effects.

--- mainCode.js
+++ mainCode.js
@@ -1,4 +1,5 @@
-let x = [5,4,8,1,7,9,10];
+
+// let x = [5,4,8,1,7,9,10];
 let print = console.log;
 const insert = document.getElementById("blockContainer");
 let elemeti;
@@ -55,7 +56,8 @@ function removeBoxes(){
                     for (j = i + 1; j < arr.length; j++) {
                         setColor(elements.item(i),'red');
                         setColor(elements.item(j),'blue');
-                        await delay(500);
+                        await delay(50);
+                        playTone(arr[j] * 800,"sawtooth",1);
                         removeColor(elements.item(i),'red');
                         removeColor(elements.item(j),'blue');
                         if (arr[j] < arr[min_idx]){
@@ -75,9 +77,16 @@ function removeBoxes(){
                     
                     swap(arr,min_idx, i); 
                 }
-                // print(x); 
+                final(arr);
 
             } 
+    }
+    async function final(arr){
+        for (i = 0;i<arr.length;i++){
+            await delay(50);
+
+            playTone(1500 + arr[i] * 10,"square",0.25);
+        }
     }
     function setColor(elementiById,color) {        
         elementiById.classList.add(color + 'Background');   
